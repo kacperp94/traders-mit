fc9dbb6c2467490eae2ee7d285501536a10be74d
Kacper Piotrowski <kptrwsk2@illinois.edu>
2017-11-05 11:04:45 -0600

    working bot

--- darkpools/run-linux.sh
+++ darkpools/run-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./utils/mangocore-linux-amd64.x --case ./samples/cases/main3.json --start 5
+./utils/mangocore-linux-amd64.x --case ./samples/cases/main1.json --start 5
--- darkpools/samples/dev_bot.py
+++ darkpools/samples/dev_bot.py
@@ -10,25 +10,36 @@ t = tt.TradersBot(host='127.0.0.1', id='trader0', password='trader0')
 tick = 0
 tickers = ['USDCAD','USDJPY','EURUSD','USDCHF','CHFJPY','EURJPY','EURCHF','EURCAD']
 marketTickers = ['USDCAD', 'EURUSD', 'USDCHF', 'USDJPY']
+darkTickers = ['CHFJPY','EURJPY','EURCHF','EURCAD']
 state = {}
-
+darkpools = {}
 
 orderbook = {}
 #cash = {'USD': 100000, 'JPY': 0, 'EUR': 0, 'CHF': 0, 'CAD': 0}
-triangs = {'USDCAD': ['EURUSD', 'EURCAD'], 'EURUSD':['USDCHF', 'EURCHF'], 'USDJPY': ['EURUSD', 'EURJPY'], 'USDCHF': ['USDJPY', 'CHFJPY']}
+triangs = {'USDCAD': ['EURUSD', 'EURCAD'], 'USDCHF':['EURUSD', 'EURCHF'], 'USDJPY': ['EURUSD', 'EURJPY']}
+
+#, 'USDCHF': ['USDJPY', 'CHFJPY']
+
 t_last_ord = 0
 time_last_clear = 0
 time_last_update = 0
 
-arb_threshold = 0.017
+old_pnl = 0
+pnl = 0
+
+
+arb_threshold = 0.007
 trans_fee = 0.0001
 
 
-#fileTrade = open("onTrade.txt", 'w')
+fileTrade = open("onTrade.txt", 'w')
 #fileonTrader = open("onTrader.txt", 'w')
 #file = open("test.txt", 'w')
 for i in tickers:
     state[i] = {}
+
+for i in darkTickers:
+    darkpools[i] = -1
 #file = open("testfile.txt","w")
 """
 EURJPY
@@ -44,13 +55,13 @@ def f(msg, order):
     idx = msg["ticker"]
     state[idx]["bids"] = msg['bids']
     state[idx]["asks"]  = msg['asks']
+    #print(tick)
     arbitrage(idx, order)
     tick+=1
 
 
 def arbitrage(idx, order):
-    global state, triangs, arb_threshold
-
+    global state, triangs, arb_threshold, darkpools
 
     try:
         idx_reg = triangs[idx][0]
@@ -62,21 +73,26 @@ def arbitrage(idx, order):
         p2a = min(state[idx_reg]['asks'].keys())
         p2b = max(state[idx_reg]['bids'].keys())
 
-        p1 = round((float(p1a)+float(p1b))/2,2)
-        p2 = round((float(p2a)+float(p2b))/2,2)
+        quantity = min(1000, min(state[idx]['asks'][p1a], state[idx_reg]['asks'][p2a]))
 
-        p3 = p1*p2-arb_threshold
-        print(p1, p2, p3)
+        p1 = round((float(p1a)+float(p1b))/2.02,2)
+        p2 = round((float(p2a)+float(p2b))/2.02,2)
+        if(darkpools[idx_dark]==-1):
+            p3 = p1*p2-arb_threshold
+            quantity = 10
+        else:
+            p3 = (darkpools[idx_dark] + p1*p2-arb_threshold)/2
+        #print(p1, p2, p3)
 
-    #quantity = min(state['USDCAD']['asks'][p1], state['EURUSD']['asks'][p2])
+
     #print(quantity)
     #p = abs(float(state['USDCAD']['asks'][0])-float(state['USDCAD']['asks'][0])) + float(state['USDCAD']['asks'][0])
 
-        order.addTrade(idx, True, 100, p1, None)
+        order.addTrade(idx, True, quantity, p1, None)
 
-        order.addTrade(idx_reg, True, 100, p2, None)
+        order.addTrade(idx_reg, True, quantity, p2, None)
 
-        order.addTrade(idx_dark, True, -100, p3, None)
+        order.addTrade(idx_dark, True, -quantity, p3, None)
 
         """order.addSell('USDCHF', quantity = 200, price = p1)
         order.addBuy('EURUSD', quantity = 200, price = p2)
@@ -93,17 +109,34 @@ def arbitrage(idx, order):
             order.addSell(idx, quantity=quantity, price=1.01)
             """
     except:
-        print('error')
+        pass
 
 
 def g(msg,order):
-    global tick
+    global tick, old_pnl, pnl
+    old_pnl = pnl
+    pnl =  str(msg['trader_state']['default_pnl'])
+    print("profit: " + pnl)
+
+    if(tick%100 > 5 and tick%100<12):
+        c = 0
+        for ob in msg['trader_state']['open_orders']:
+            print(ob)
+            if(c==3):
+                break
+            order.addCancel(msg['trader_state']['open_orders'][ob]['ticker'], (msg['trader_state']['open_orders'][ob]['order_id']))
+            c+=1
+
     #fileonTrader.write("tick: " + str(tick)+ " \n {}".format(str(msg) + "\n"))
     tick+=1
 
 def k(msg, order):
-    global tick
-    #fileTrade.write("tick: " + str(tick)+ " \n {}".format(str(msg) + "\n"))
+    global tick, darkpools
+    #print(msg)
+    for ob in msg['trades']:
+        if ob['ticker'] in darkTickers:
+            darkpools[ob['ticker']] = float(ob['price'])
+    fileTrade.write("tick: " + str(tick)+ " \n {}".format(str(msg) + "\n"))
     tick+=1
 """def arbit(msg, order):
     global tick
